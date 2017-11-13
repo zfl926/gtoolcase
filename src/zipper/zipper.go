cead3b8c5b5bdccdf1a5d498a2b11d20918261c4
pulei.zhou <pulei.zhou@hpe.com>
2017-11-13 17:57:23 +0800

    add new method

--- src/zipper/zipper.go
+++ src/zipper/zipper.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"os"
-	"zip"
-	"fmt"
+    "io"
+	"archive/zip"
+	"flag"
+    //"io/ioutil"
+    "fmt"
 )
 
 type Compress interface {
@@ -15,18 +18,23 @@ type Decompress interface {
 }
 
 type ZipCompress struct {
+    IgnoreFiles            string            // the file to ignore
+}
+
+type ZipDecompress struct {
 
 }
 
+// compress
 func (this *ZipCompress)Compress(files []*os.File, dst string) error {
 	d, _ := os.Create(dst)
 	w := zip.NewWriter(d)
     defer func () {
+        w.Close()
     	d.Close()
-    	w.Close()
-    } 
+    }() 
     for _, file := range files {
-        err := this.DoCompress(file, "", w)
+        err := this.addToZip(file, "", w)
         if err != nil {
             return err
         }
@@ -34,7 +42,7 @@ func (this *ZipCompress)Compress(files []*os.File, dst string) error {
     return nil
 }
 
-func (this *ZipCompress)DoCompress(file *os.File, prefix string, zw *zip.Writer) error {
+func (this *ZipCompress)addToZip(file *os.File, prefix string, zw *zip.Writer) error {
     info, err := file.Stat()
     if err != nil {
         return err
@@ -50,7 +58,7 @@ func (this *ZipCompress)DoCompress(file *os.File, prefix string, zw *zip.Writer)
             if err != nil {
                 return err
             }
-            err = compress(f, prefix, zw)
+            err = this.addToZip(f, prefix, zw)
             if err != nil {
                 return err
             }
@@ -58,22 +66,67 @@ func (this *ZipCompress)DoCompress(file *os.File, prefix string, zw *zip.Writer)
     } else {
         header, err := zip.FileInfoHeader(info)
         header.Name = prefix + "/" + header.Name
-        if err != nil {
-            return err
-        }
+        fmt.Println(header.Name)
         writer, err := zw.CreateHeader(header)
         if err != nil {
             return err
         }
         _, err = io.Copy(writer, file)
+        if err != nil {
+            fmt.Printf("error")
+            return err
+        }
         file.Close()
+    }
+    return nil
+}
+
+// decompress
+func (this *ZipDecompress) Decompress(zipfile string, dst string) error{
+    r, err := zip.OpenReader(zipfile)
+    if err != nil {
+        return err
+    }
+    defer r.Close()
+    for _,f := range r.File {
+        fmt.Printf("文件名 %s:\n", f.Name)
+        rc, err := f.Open()
         if err != nil {
             return err
         }
+        // _, err = io.CopyN(os.Stdout, rc, int64(f.UncompressedSize64))
+        // if err != nil {
+        //     return err
+        // }
+        rc.Close()
+        //fmt.Println()
     }
     return nil
 }
 
 func main(){
-	fmt.Println("hello world!")
+	files := flag.String("files",".","zip files")
+    zipfile := flag.String("zipfile",".","zip file name")
+
+    flag.Parse()
+
+    fmt.Printf(" cmd   : %s\n", flag.Arg(0))
+    fmt.Printf(" flies : %s\n", *files)
+    fmt.Printf(" zipfile : %s\n", *zipfile)
+
+    var folder string = "./test"
+    var output string = "test.zip"
+    file, err := os.Open(folder)
+    if err != nil {
+        fmt.Println(err)    
+        return
+    }
+    var zippedFiles []*os.File = []*os.File{file}
+    compress := &ZipCompress{}
+    e := compress.Compress(zippedFiles, output)
+    if e != nil {
+        fmt.Println(e)
+    }
+    // decompress := &ZipDecompress{}
+    // decompress.Decompress("./test.zip", "")
 }
