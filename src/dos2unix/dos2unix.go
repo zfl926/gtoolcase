b282b98d58b7b2f00a95aba2075942c951d907e2
zfl926 <zfl926@gmail.com>
2018-09-16 16:34:07 +0800

    add

--- src/dos2unix/dos2unix.go
+++ src/dos2unix/dos2unix.go
@@ -11,7 +11,22 @@ import (
 
 type Cmd struct {
 	FileNames            []string               `file names`
-	Format               int                    `format 1 dos2unix 2 unix2dos`
+	Format               int                     `format 1 dos2unix 2 unix2dos`
+}
+
+type CmdExcutor interface {
+    Execute()
+}
+
+
+func (this *Cmd) Execute(){
+    for _, fileName := range this.FileNames {
+        if this.Format == 1 {
+            dos2unix(fileName)
+        } else {
+            dos2unix(fileName)
+        }
+    }
 }
 
 func dos2unix(fileName string) int {
@@ -40,6 +55,32 @@ func dos2unix(fileName string) int {
     return 1
 }
 
+func unix2dos(fileName string) int {
+    rf, err := os.Open(fileName)
+    if err != nil {
+        return -1
+    }
+    wf, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+    if err != nil {
+        return -1
+    }
+    defer rf.Close()
+    rd := bufio.NewReader(rf)
+    wd := bufio.NewWriter(wf)
+    var strlines string
+    for {
+        line, err := rd.ReadString('\n')
+        if err != nil || io.EOF == err {
+            break
+        }
+        line = strings.Replace(line, "\n", "\r\n", -1)
+        strlines = strlines + line
+    }
+    wd.WriteString(strlines)
+    wd.Flush()
+    return 1
+}
+
 
 func cmdController() {
     format := flag.String("format", "dos2unix", "convert format")
@@ -49,11 +90,17 @@ func cmdController() {
     	strFileName := fmt.Sprintf("%s", *fileNames)
     	strFormat := fmt.Sprintf("%s", *format)
     	fileName := strings.Split(strFileName, " ")
-    	for _, file := range fileName {
-    		if strFormat == "dos2unix" {
-    			dos2unix(fmt.Sprintf("%s", file))
-    		}
-    	}
+        var intFormat int = 1
+        if strFormat == "dos2unix" {
+            intFormat = 1
+        } else {
+            intFormat = 2
+        }
+        var cmd Cmd = Cmd{
+            FileNames : fileName,
+            Format    : intFormat,
+        }
+        cmd.Execute()
     }
 }
 
